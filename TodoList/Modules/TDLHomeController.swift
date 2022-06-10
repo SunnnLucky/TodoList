8ecd66684d06f8c1e76883bdb32542b1590e1354
SunnnLucky <455319106@qq.com>
2022-06-10 14:42:06 +0800

    add UIBarButtonItem

--- TodoList/Modules/TDLHomeController.swift
+++ TodoList/Modules/TDLHomeController.swift
@@ -17,10 +17,26 @@ class TDLHomeController: TDLBaseTVController {
         title = "Todoey"
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
         tableView.backgroundColor = #colorLiteral(red: 0.9306189418, green: 0.7211485505, blue: 0, alpha: 1)
+        
+        let rightBtn = UIBarButtonItem(barButtonSystemItem: .add, target: self, action:#selector(addButtonPressed))
+        rightBtn.tintColor = .white
+        navigationItem.rightBarButtonItem = rightBtn
+    }
+    
+    //MARK: - Add New Items
+    @objc func addButtonPressed() {
+        let alert = UIAlertController(title: "Add New Todoey Item", message: "", preferredStyle: .alert)
+        let addAction = UIAlertAction(title: "Add Item", style: .default) { action in
+            
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        alert.addAction(cancelAction)
+        alert.addAction(addAction)
+        present(alert, animated: true, completion: nil)
     }
 }
 
-// MARK: - Table view data source
+// MARK: - Table view data source & delegate
 extension TDLHomeController {
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -34,5 +50,13 @@ extension TDLHomeController {
         let cell = tableView.dequeueReusableCell(withIdentifier: cellID, for: indexPath)
         cell.textLabel?.text = itemArray[indexPath.row]
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        guard let cell = tableView.cellForRow(at: indexPath) else {return}
+        cell.accessoryType = cell.accessoryType == .checkmark ? .none : .checkmark
+        
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }

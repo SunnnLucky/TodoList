db01d629bf27f4b07ac48e82ea87db21fead925f
SunnnLucky <455319106@qq.com>
2022-06-28 21:48:51 +0800

    large titles

--- TodoList/Base/TDLBaseNavController.swift
+++ TodoList/Base/TDLBaseNavController.swift
@@ -17,7 +17,7 @@ class TDLBaseNavController: UINavigationController {
         let appearance = UINavigationBarAppearance()
         appearance.configureWithOpaqueBackground()
         appearance.backgroundColor = .systemBlue
-//        appearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
+        appearance.titleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
         appearance.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor : UIColor.white]
         
         navigationBar.prefersLargeTitles = true
